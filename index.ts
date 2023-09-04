a890d4a78b0f8ea55e6238ad75bdc8d9ddd7e92b
asmaahmed <asmaahmed024590@gmail.com>
2023-09-04 14:23:27 +0500

    updated

--- index.ts
+++ index.ts
@@ -1,12 +1,19 @@
 import inquirer from "inquirer";
 
-const systemGeneratedNo = Math.floor(Math.random() * 10);
+const systemGeneratedNo = Math.floor(Math.random() * 10+ 1 );
 
-let numTries: number = 3
+let numTries: number = 3;
+let play: boolean = true;
+
 type ansType = {
     userGuess: number
 }
 
+type playAgainAnswerType = {
+    playAgain: boolean
+}
+
+while (play) {
 while (numTries > 0) {
 
 const answers : ansType = await inquirer.prompt([
@@ -24,9 +31,34 @@ console.log(`You have ${numTries- 1} left `)
 
 if(answers.userGuess === systemGeneratedNo){
     console.log("Yess your answer is correct \n You Win!!")
-} else {
-    console.log("Please try again Better luck next time!")
+    numTries = 0
+} 
+else {
+    console.log("Please try again Better luck next time!");
+    if (systemGeneratedNo > answers.userGuess) console.log("Think higher");
+    else{
+        console.log("Think lower");
+    
+    }
 }
   numTries--;
 }
-// asma 
+}
+  const playAgainAnswer = await inquirer.prompt ([
+    {
+        type: "confirm",
+        name:"playAgain",
+        message: "Wanna play again",
+    },
+  ]); 
+  console.log("playAgain",playAgainAnswer);
+  if (playAgainAnswer.playAgain) {
+    numTries = 3;
+    const systemGeneratedNo = Math.floor(Math.random() * 10+ 1 );
+  } else {
+    console.log("Exitting game.....")
+    play = false;
+  }
+
+
+
